eaf5ba2d12581ae2d86337b8d7120c5b981c4ed3
Rashmi Phegade <167325347+phegaderashmi@users.noreply.github.com>
2024-07-02 11:15:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let inputs = document.getElementById("inp");
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("Please Enter task")
+    }else{
+        let newElement = document.createElement("ul");
+        newElement.innerHTML=`${inputs.value}<i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newElement);
+        inputs.value="";
+        newElement.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newElement.remove()
+        }
+    }
+
+}
